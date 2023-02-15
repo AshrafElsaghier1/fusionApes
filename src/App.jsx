13f62138667ba02c2df216a6baa9a5b0a750b781
Your Name <you@example.com>
2023-02-15 22:31:01 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-blend-color-dodge border">
+      <h1 className=" font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "main-color": "#EDB731",
+      "bg-smoke-white": "#FBFBFB",
+      "text-gray": "#AFAFAF",
+      "text-color": "#222",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
